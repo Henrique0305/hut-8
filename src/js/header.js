3a4a89462f5bba4fd52fb75de73c5a4d91574db6
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2023-06-22 22:52:43 -0300

    Adicionando JS to header & styles to products

---
+++ src/js/header.js
@@ -0,0 +1,15 @@
+const url = window.location.href;
+
+const header_links = document.querySelectorAll(".nav-link");
+
+const arrayLinks = Array.from(header_links);
+
+const current_link = arrayLinks.find((link) => {
+  const href = link.getAttribute("href").replace(".", "");
+
+  console.log(href);
+
+  return url.includes(href);
+});
+if (current_link) current_link.classList.add("active");
+
